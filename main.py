6bcaed4e8d67975cc8f055bb694d8ca2556711f8
S Adithya Nachiyappan <adithyanachiyappan@gmail.com>
2026-02-20 10:44:11 +0530

    Initialized DB

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello from WARMS Backend"}
+@app.get("/ping")
+async def ping():
+    return{"message":"Health verified: Backend running"}
